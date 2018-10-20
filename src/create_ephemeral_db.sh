1c48732cf621318500968ecab8c00802c29981ff
Marcel Wiget <mwiget@gmail.com>
2018-10-20 10:42:38 +0200

    add interface description for linux xe* interfaces

--- src/create_ephemeral_db.sh
+++ src/create_ephemeral_db.sh
@@ -4,24 +4,30 @@
 #
 # create eDB with interface description taken from docker
 
-iflist=""
-netlist=$(docker inspect --format='{{range $p, $conf := .NetworkSettings.Networks}} {{$p}}  {{end}}' $HOSTNAME || echo "")
+# start with existing xe interfaces (used by vmx-docker-lwaftr)
 
-if [ -z "$netlist" ]; then
-  exit 0
-fi
+netlist=$(ip link|grep ' xe'| cut -d' ' -f2 | cut -d: -f1)
+index=0
 
 echo "configure ephemeral openjnpr-container-vmx-vfp0"
 
-index=-1
 for net in $netlist; do
-  if [ "-1" -eq "$index" ]; then
-    ifd="fxp0"
-  else
-    ifd="ge-0/0/$index"
-    ethindex=$(($index + 1))
-    ip addr flush dev eth${ethindex}
-  fi
+  ifd="ge-0/0/$index"
+  cat <<EOF
+set interfaces $ifd description "$net"
+EOF
+  index=$(($index + 1))
+done
+
+# docker networks follow next, attached to eth0, eth1, ...
+
+netlist=$(docker inspect --format='{{range $p, $conf := .NetworkSettings.Networks}} {{$p}}  {{end}}' $HOSTNAME || echo "")
+netlist=$(echo $netlist | cut -d' ' -f2-)   # remove first net, used for fxp0 already
+
+for net in $netlist; do
+  ifd="ge-0/0/$index"
+  ethindex=$(($index + 1))
+  ip addr flush dev eth${ethindex}
   cat <<EOF
 set interfaces $ifd description "$net"
 EOF
