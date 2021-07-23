f90fade93c785617f9c365dbc1c7de0287cc1982
Steffen Ries <sries@juniper.net>
2021-07-23 12:47:30 -0700

    set IO_CORE, fix typo

--- src/update_core_mapping.sh
+++ src/update_core_mapping.sh
@@ -11,7 +11,7 @@ write_intf_core_mapping()
     fpc_intf_type="af_packet"
     ix_port=$1
     intf=$2
-    io_cpu="1"
+    io_cpu="$3"
     line_num="2"
     line_num=$(expr $line_num + $(cat  ${core_mapping_file}.cfg |grep ix_port | wc -l))
     if grep -qe "^$intf" ${core_mapping_file}.cfg; then
@@ -22,9 +22,10 @@ write_intf_core_mapping()
 }
 
 core_mapping_file="/usr/share/pfe/core_mapping"
+io_core="${IO_CORE:-1}"
 
 ix_port=0
 for intf in $(ls /sys/class/net | grep 'eth[1-9][0-9]*'); do
-    write_intf_core_mapping $ix_port $intf
+    write_intf_core_mapping $ix_port $intf $io_core
     ((ix_port += 1))
 done
